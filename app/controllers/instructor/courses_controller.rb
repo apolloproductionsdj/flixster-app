f86ebc26f4a69011e43b641b46649806a4ce4b33
Mike Torres <apolloproductionsdj@gmail.com>
2019-04-22 23:30:25 +0000

    issues with Lessons Controller

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -1,38 +1,37 @@
 class Instructor::CoursesController < ApplicationController
- before_action :authenticate_user!
- before_action :require_authorized_for_current_course, only: [:show]
- def new
-  @course = Course.new
- end 
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_course, only: [:show]
 
- def create
-  @course = current_user.courses.create(course_params)
-  if @course.valid?
-    redirect_to instructor_course_path(@course)
-  else
-    render :new, status: :uprocessable_entity
+  def new
+    @course = Course.new
   end
- end
 
- def show
- end 
+  def create
+    @course = current_user.courses.create(course_params)
+    if @course.valid?
+      redirect_to instructor_course_path(@course)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
- def require_authorized_for_current_course
-   if current_course.user != current_user
-    render plain: "Unauthorized", status: :unauthorized 
-   end 
- end
+  def show
+  end
 
+  private
 
- private
+  def require_authorized_for_current_course
+    if current_course.user != current_user
+      render plain: "Unauthorized", status: :unauthorized
+    end
+  end
 
   helper_method :current_course
   def current_course
-    @current_course ||= Course.find(params[:course_id])
+    @current_course ||= Course.find(params[:id])
   end
 
- def course_params
-  params.require(:course).permit(:title, :description, :cost)
- end
-
+  def course_params
+    params.require(:course).permit(:title, :description, :cost)
+  end
 end
