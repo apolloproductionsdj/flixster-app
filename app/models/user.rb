0692519a6a5a32950ca9c49e7b6b99b5dbf56813
Mike Torres <apolloproductionsdj@gmail.com>
2019-04-29 15:34:52 +0000

    enrollment button current_user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
  
   has_many :courses
   has_many :enrollments
-  has_many :enrolled_course, through: :enrollments, source: :course 
+  has_many :enrolled_courses, through: :enrollments, source: :course 
  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -12,6 +12,6 @@ class User < ApplicationRecord
   def enrolled_in?(course)
     return enrolled_courses.include?(course)
   end
-  
+
 end 
 
