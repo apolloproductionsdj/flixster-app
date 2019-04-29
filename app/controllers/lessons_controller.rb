3bceedc43dba91c7d6285e680b9f6cdfd89a1e5d
Mike Torres <apolloproductionsdj@gmail.com>
2019-04-29 17:08:26 +0000

    required_authorized

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,7 +1,7 @@
 class LessonsController < ApplicationController
   before_action :authenticate_user!
-    
-    
+  before_action :require_authorized_for_current_lesson.section.course, only: [:show]
+
   def show
   end
 
