0d6d0e5c19ef6f320b2f290a8e3bb8b4e37230a2
Mike Torres <apolloproductionsdj@gmail.com>
2019-04-30 03:16:23 +0000

    lessons_include? flash alert

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -2,12 +2,23 @@ class LessonsController < ApplicationController
   before_action :authenticate_user!
   before_action :require_authorized_for_current_lesson, only: [:show]
 
+  def create
+    flash[:notice] = "You Must Be Enrolled"
+    redirect_to course_path(current_lesson)
+  end
+
   def show
     current_lesson.section.course(lesson: current_lesson)
+    redirect_to course_path(current_lesson) 
   end
 
   private
 
+def require_authorized_for_current_lesson
+   if current_lesson.user != current_user
+    flash :notice "You Must Be Enrolled"
+   end 
+ end 
 
 
   helper_method :current_lesson
